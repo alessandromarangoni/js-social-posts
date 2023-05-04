239030b14548bb33c10938968b8d5905b1230290
alessandro marangoni <alessandromarangoni980@gmail.com>
2023-05-04 17:29:38 +0200

    ricomincio creo il primo post fisso

--- js/main.js
+++ js/main.js
@@ -63,40 +63,43 @@ for (let i = 0; i < posts.length; i++) {
 }
 
 //crearci un post
-let container = document.getElementById("container")
-console.log(container)
+let container = document.getElementById("container");
+console.log(container);
+let post = `<div class="post">
+<div class="post__header">
+    <div class="post-meta">                    
+        <div class="post-meta__icon">
+            <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+        </div>
+        <div class="post-meta__data">
+            <div class="post-meta__author">Phil Mangione</div>
+            <div class="post-meta__time">4 mesi fa</div>
+        </div>                    
+    </div>
+</div>
+<div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+<div class="post__image">
+    <img src="https://unsplash.it/600/300?image=171" alt="">
+</div>
+<div class="post__footer">
+    <div class="likes js-likes">
+        <div class="likes__cta">
+            <a class="like-button  js-like-button" href="#" data-postid="1">
+                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                <span class="like-button__label">Mi Piace</span>
+            </a>
+        </div>
+        <div class="likes__counter">
+            Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+        </div>
+    </div> 
+</div>            
+</div>`
 
-function creapost() {
-    // container post
-    let postContainer = document.createElement("div")
-    postContainer.classList.add("post")
-    console.log(postContainer)
-    // header post
-    let postHeader = document.createElement("div")
-    creaClasse(postHeader,"post__header")
-    console.log(postHeader)
-    // postMeta
-    let postMeta =document.createElement("div")
-    creaClasse(postMeta,"post-meta")
-    console.log(postMeta)
-    //postMeta__icon 
-    let postMeta__icon =document.createElement("div")
-    creaClasse(postMeta__icon,"post-meta__icon")
-    console.log(postMeta__icon)
-    let imgProfilo = document.createElement("img")
-    imgProfilo.src=posts[0].author["image"]
+
+    container.innerHTML += post;
 
 
-// append
-    appendo(container,postContainer)
-    appendo(postContainer,postHeader)
-    appendo(postHeader,postMeta)
-    appendo(postMeta,postMeta__icon)
-    appendo(postMeta__icon, imgProfilo)
-
-}
-
-creapost()
 
 function creaClasse(oggetto, valore) {
     oggetto.classList.add(valore)
